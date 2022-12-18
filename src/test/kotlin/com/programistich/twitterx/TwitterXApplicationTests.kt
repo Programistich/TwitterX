80ad03db31ba32b01866091cca1bbaaefe661042
Dzhos Oleksii <35292229+Programistich@users.noreply.github.com>
2022-12-18 15:08:27 +0200

    Init Project (#1)
    **Background**
    * Check github flow
    
    **Changes**
    
    * Edit android test to gradle tests
    * Sample test
    
    **Test plan**
    
    Create Pull Request

--- src/test/kotlin/com/programistich/twitterx/TwitterXApplicationTests.kt
+++ src/test/kotlin/com/programistich/twitterx/TwitterXApplicationTests.kt
@@ -1,6 +1,13 @@
-// package com.programistich.twitterx
-//
-// import org.springframework.boot.test.context.SpringBootTest
-//
-// @SpringBootTest
-// class TwitterXApplicationTests
+package com.programistich.twitterx
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TwitterXApplicationTests {
+
+    @Test
+    fun contextLoads() {
+        assert(4 == 2 + 2)
+    }
+}
